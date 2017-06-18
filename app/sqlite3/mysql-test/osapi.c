42618d7a764c1d69eaa77f66b94ab0a2184fc673
Luiz Felipe Silva <gituser@luizfelipe.eng.br>
2017-06-18 00:18:02 -0300

    store updated test environment

--- app/sqlite3/mysql-test/osapi.c
+++ app/sqlite3/mysql-test/osapi.c
@@ -1,7 +1,10 @@
-#include <stdlib.h>
+#include <c_stdlib.h>
 #include <malloc.h>
 #include <time.h>
 #include "user_interface.h"
+
+#undef dbg_printf
+#define dbg_printf(...) 0
 
 unsigned long system_get_free_heap_size_integer = 48 * 1024;
 
@@ -16,19 +19,25 @@ int os_get_random(unsigned char *buf, size_t len)
 
 void *os_malloc(size_t size)
 {
-	system_get_free_heap_size_integer+=size;
+	long before = system_get_free_heap_size();
+	system_get_free_heap_size_integer-=size;
+	dbg_printf("os_malloc: Before %ld, after %ld\n", before, system_get_free_heap_size());
 	return malloc (size);
 }
 
 void os_free(void *ptr)
 {
-	system_get_free_heap_size_integer-=malloc_usable_size(ptr);
+	long before = system_get_free_heap_size();
+	system_get_free_heap_size_integer+=malloc_usable_size(ptr);
+	dbg_printf("os_free: Before %ld, after %ld\n", before, system_get_free_heap_size());
 	return free(ptr);
 }
 
 void *os_realloc(void *ptr, size_t size)
 {
-	system_get_free_heap_size_integer-=malloc_usable_size(ptr);
-	system_get_free_heap_size_integer+=size;
+	long before = system_get_free_heap_size();
+	system_get_free_heap_size_integer+=malloc_usable_size(ptr);
+	system_get_free_heap_size_integer-=size;
+	dbg_printf("os_realloc: Before %ld, after %ld\n", before, system_get_free_heap_size());
 	return realloc(ptr, size);
 }
